ce68e17fdce273bf118f51f998333215a1c46474
lodashreload <icarusjapan@gmail.com>
2022-03-02 22:03:58 -0500

    all but adventure mode done

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import './style.css'
 const numberOfCupsOfCoffee = 3
 const fullName = 'Nicholas Amheiser'
-console.log(`My name is ${fullName} and I drink ${numberOfCupsOfCoffee}`)
+console.log(
+  `My name is ${fullName} and I drink ${numberOfCupsOfCoffee} cups of coffee`
+)
 
 const aboutMe = {
   fullName: fullName,
@@ -11,11 +13,11 @@ const aboutMe = {
 
 console.table(aboutMe)
 
-const userName = window.prompt
+const userName = window.prompt('What is your user name?')
 console.log(userName)
-
-const firstOperand = window.prompt
-const secondOperand = window.prompt
+// ----------------------------------- Math -----------------------------------
+const firstOperand = window.prompt('Please write one number.')
+const secondOperand = window.prompt('Please write a second number.')
 const firstNum = Number(firstOperand)
 const secondNum = Number(secondOperand)
 
@@ -33,3 +35,22 @@ console.log(quotient)
 
 const remainder = firstNum % secondNum
 console.log(remainder)
+
+// ---------------------------------------------arrays --------------------------------
+type stats = {
+  smallest: 'smallest'
+  largest: 'largest'
+  sum: 'arraySum'
+  average: 'average'
+}
+const numbers = [
+  17235, 49260, 44379, 49202, 20771, 16378, 49436, 45930, 2748, 45130,
+]
+console.log(numbers)
+let smallest = Math.min(...numbers)
+let largest = Math.max(...numbers)
+console.log(smallest + largest)
+let arraySum = numbers.reduce((a, b) => a + b, 0)
+console.log(arraySum)
+let average = arraySum / numbers.length || 0
+console.log(average)
