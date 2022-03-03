ef5812f2b9a5e263a78db9fd54db9f2877af9985
lodashreload <icarusjapan@gmail.com>
2022-03-02 21:09:23 -0500

    Something is wrong help Greg

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import './style.css'
+const numberOfCupsOfCoffee = 3
+const fullName = 'Nicholas Amheiser'
+console.log(`My name is ${fullName} and I drink ${numberOfCupsOfCoffee}`)
+
+const aboutMe = {
+  fullName: fullName,
+  luckyNumber: 3,
+  favoriteMovie: ['Fifth Element', 'John Wick', 'Kung Fu Hustle'],
+}
+
+console.table(aboutMe)
+
+const userName = window.prompt
+console.log(userName)
+
+const firstOperand = window.prompt
+const secondOperand = window.prompt
+const firstNum = Number(firstOperand)
+const secondNum = Number(secondOperand)
